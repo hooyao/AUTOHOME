37fb57e992803592435c562f93e017da2ede4e96
Yao Hu <huy@tradeshift.com>
2017-10-06 09:16:05 +0800

    add dnsforwarder to improve stability

--- chinadnswatch.py
+++ chinadnswatch.py
@@ -19,11 +19,20 @@ testHosts = ['www.google.com',
 
 client = docker.from_env()
 
+dnsnetwork = 'dnsnet'
+
 
 def main(*args):
     while True:
+        check_dns_network()
         check_dns_status()
         time.sleep(20)
+
+
+def check_dns_network():
+    dnsnet_list = list(filter(lambda network: network.name == 'dnsnet', client.networks.list()))
+    if len(dnsnet_list) == 0:
+        client.networks.create(dnsnetwork, driver='bridge')
 
 
 def check_dns_status():
@@ -32,16 +41,18 @@ def check_dns_status():
         illegal_container_list = list(
             filter(lambda container: container.name != 'chinadns' and
                                      list(filter(lambda key: '53' in key,
-                                                 container_list[0].attrs['NetworkSettings'][
+                                                 container.attrs['NetworkSettings'][
                                                      'Ports'].keys())), container_list))
         for container in illegal_container_list:
             container.remove(force=True)
 
         chinadns_container_list = list(filter(lambda container: container.name == 'chinadns', container_list))
+        dnsforwarder_container_list = list(filter(lambda container: container.name == 'dnsforwarder', container_list))
         if_restart_container = False
-        if len(chinadns_container_list) > 0:
+        if len(chinadns_container_list) > 0 and len(dnsforwarder_container_list) > 0:
             chinadns_container = chinadns_container_list[0]
-            if chinadns_container.status == 'running':
+            dnsforwarder_container = dnsforwarder_container_list[0]
+            if chinadns_container.status == 'running' and dnsforwarder_container.status == 'running':
                 for host in testHosts:
                     ifResolve = resolve(host)
                     if not ifResolve:
@@ -54,9 +65,10 @@ def check_dns_status():
                     time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                     s.substitute(time=time_str)
                     chinadns_container.restart(timeout=30)
+                    dnsforwarder_container.restart(timeout=30)
                     logger.warning('Restart Chinadns.')
             else:
-                chinadns_container.remove(force=True)
+                cleanup_containers(chinadns_container_list, dnsforwarder_container_list)
                 run_chinadns()
         else:
             run_chinadns()
@@ -65,12 +77,25 @@ def check_dns_status():
 
 
 def run_chinadns():
+    client.containers.run('chenhw2/hev-dns-forwarder:latest',
+                          name='dnsforwarder',
+                          command='-p 53 -s 8.8.8.8:53',
+                          network=dnsnetwork,
+                          detach=True)
     client.containers.run('daocloud.io/hooyao/docker-chinadns:latest', ports={'53/udp': 53},
                           name='chinadns',
                           entrypoint='chinadns',
-                          command='-s 114.114.114.114,8.8.8.8,8.8.4.4',
+                          command='-m -c /usr/local/share/chnroute.txt -s 223.5.5.5,dnsforwarder',
+                          network=dnsnetwork,
                           detach=True)
     logger.info('Run chinadns.')
+
+
+def cleanup_containers(chinadns_container_list, dnsforwarder_container_list):
+    for container in chinadns_container_list:
+        container.remove(force=True)
+    for container in dnsforwarder_container_list:
+        container.remove(force=True)
 
 
 def resolve(addr):
